4fad0870f7fe8886a92ae52d8cc0b8f7e074e5ff
roflolmao <164321647+roflolmao@users.noreply.github.com>
2024-04-04 16:06:59 +0700

    Create demo2.c

---
+++ u8g2 Library/demo2.c
@@ -0,0 +1,19 @@
+#define U8G2_WITH_UNICODE
+#include <Arduino.h>
+#include <U8g2lib.h>
+
+//U8G2_SSD1306_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE);
+U8G2_SH1106_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE);
+
+void setup(void) {
+  u8g2.begin();
+}
+
+void loop(void) {
+  u8g2.firstPage();
+  do {
+  u8g2.setFont(u8g2_font_etl14thai_t ); 
+  u8g2.drawStr(8,37, "Hello World!");  
+  } while ( u8g2.nextPage() );
+  delay(1000);
+ }
