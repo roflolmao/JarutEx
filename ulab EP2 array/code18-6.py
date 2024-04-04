6e4ff5206507742d7d301a120535fb34f5920a2d
roflolmao <164321647+roflolmao@users.noreply.github.com>
2024-04-04 13:44:01 +0700

    Create code18-6.py

---
+++ ulab EP2 array/code18-6.py
@@ -0,0 +1,8 @@
+# code18-6
+import ulab as np
+a = np.array([1,2,3,4,5], dtype=np.int16)
+b = np.array(a,dtype=np.float)
+c = np.array(b,dtype=np.int16)
+print("a is {}".format(a))
+print("b is {}".format(b))
+print("c is {}".format(c))
