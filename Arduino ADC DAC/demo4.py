86e7f3986b817b0565b241e5cc57cb783c2a9b04
roflolmao <164321647+roflolmao@users.noreply.github.com>
2024-04-05 13:39:18 +0700

    Create demo4.py

---
+++ Arduino ADC DAC/demo4.py
@@ -0,0 +1,23 @@
+#include <Arduino.h>
+
+#define pinSpk 26
+#define pinMic 36
+int adcValue;
+
+void setup() {
+  Serial.begin(115200);
+  analogReadResolution(12);
+}
+
+void loop() {
+  for (int i = 0; i < 255; i++) {
+    dacWrite( pinSpk, i );
+    adcValue = analogRead(pinMic);
+    Serial.println(adcValue);
+  }
+  for (int i = 254; i >= 0; i--) {
+    dacWrite( pinSpk, i );
+    adcValue = analogRead(pinMic);
+    Serial.println(adcValue);
+  }
+}
