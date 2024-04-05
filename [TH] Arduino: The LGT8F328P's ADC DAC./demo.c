8324a19ccdf50216fdfceff23f3b105dc901b89c
roflolmao <164321647+roflolmao@users.noreply.github.com>
2024-04-05 13:41:21 +0700

    Create demo.c

---
+++ [TH] Arduino: The LGT8F328P's ADC DAC./demo.c
@@ -0,0 +1,23 @@
+#include <Arduino.h>
+
+#define pinSpk DAC0 // D4
+#define pinMic A0
+#define MAX_VALUE 255
+#define ADC_BITS 12
+
+int adcValue;
+
+void setup() {
+  Serial.begin(9600);
+  analogReadResolution(ADC_BITS);
+  analogReference(DEFAULT);  // 5v
+  pinMode(DAC0, ANALOG);
+}
+
+void loop() {
+  for (int i = 0; i < MAX_VALUE+1; i ++) {
+    analogWrite( pinSpk, i);
+    adcValue = analogRead(pinMic);
+    Serial.println(adcValue);
+  }
+}
