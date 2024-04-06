a976da2f5870a5f84431988d57e5c28ba2c84966
roflolmao <164321647+roflolmao@users.noreply.github.com>
2024-04-06 12:26:33 +0700

    Create demo.c

---
+++ PIC18F458 Ep.5 GPIO and 7-Segments/demo.c
@@ -0,0 +1,61 @@
+#pragma config OSC = HS 
+#pragma config OSCS = ON
+#pragma config PWRT = OFF 
+#pragma config BOR = ON  
+#pragma config BORV = 25 
+#pragma config WDT = OFF  
+#pragma config WDTPS = 128 
+#pragma config STVR = ON 
+#pragma config LVP = ON 
+#pragma config CP0 = OFF 
+#pragma config CP1 = OFF
+#pragma config CP2 = OFF
+#pragma config CP3 = OFF
+#pragma config CPB = OFF
+#pragma config CPD = OFF 
+#pragma config WRT0 = OFF  
+#pragma config WRT1 = OFF
+#pragma config WRT2 = OFF  
+#pragma config WRT3 = OFF   
+#pragma config WRTC = OFF
+#pragma config WRTB = OFF 
+#pragma config WRTD = OFF   
+#pragma config EBTR0 = OFF 
+#pragma config EBTR1 = OFF 
+#pragma config EBTR2 = OFF
+#pragma config EBTR3 = OFF 
+#pragma config EBTRB = OFF   
+
+#include <xc.h>
+
+#define _XTAL_FREQ 20000000 
+
+unsigned char seg_pattern = 0b00000001;
+
+void setup() {
+    TRISC = 0x00; // 0b00000000
+    TRISD = 0x00; // 0b00000000
+}
+
+void loop() {
+    // disable
+    PORTD = 0b00000000;
+    // transfer
+    PORTC = seg_pattern;
+    seg_pattern <<= 1;
+    if (seg_pattern == 0x00) {
+        seg_pattern = 0x01;
+    }
+    // enable
+    PORTD = 0b00000001;
+    // delay
+    __delay_ms(100);   
+}
+
+void main(void) {
+    setup();
+    while (1) {
+        loop();
+    }
+    return;
+}
