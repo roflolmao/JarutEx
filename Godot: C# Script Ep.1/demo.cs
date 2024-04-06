a43bcd515516ac478571f6ff779b99f3d4f0a63f
roflolmao <164321647+roflolmao@users.noreply.github.com>
2024-04-06 12:55:03 +0700

    Create demo.cs

---
+++ Godot: C# Script Ep.1/demo.cs
@@ -0,0 +1,27 @@
+using Godot;
+using System;
+public class Icon : Sprite
+{
+	int num1, num2, direction;
+public override void _Ready() {
+	num1 = 2;
+	num2 = 0;
+	direction = 0;
+}
+public override void _Process(float delta)
+{
+	float result = (float)Math.Pow(num1, num2);
+	Position = new Vector2(num2*30, 100+(float)(result/5000000));
+	if (direction == 0) {
+		num2 += 1;
+		if (num2 > 30) {
+			direction = 1;
+		}
+	} else {
+		num2 -= 1;
+		if (num2 == 0) {
+			direction = 0;
+		}
+	}
+}
+}
