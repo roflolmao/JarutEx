2db881881bf4bb88c80449d970755ef798d87f31
roflolmao <164321647+roflolmao@users.noreply.github.com>
2024-04-06 12:57:52 +0700

    Create program1-4.cs

---
+++ พื้นฐาน C#/program1-4.cs
@@ -0,0 +1,21 @@
+using System;
+namespace if_else
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x = 10.0;
+
+
+            if (x == 0.0)
+            {
+                Console.WriteLine(" x == 0 ");
+            }
+            else
+            {
+                Console.WriteLine(" x != 0 ");
+            }
+        }
+    }
+}
