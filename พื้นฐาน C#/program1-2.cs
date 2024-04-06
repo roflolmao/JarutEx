68e248d132dbb893ddc5cd8b7f074abf0870263d
roflolmao <164321647+roflolmao@users.noreply.github.com>
2024-04-06 12:57:16 +0700

    Create program1-2.cs

---
+++ พื้นฐาน C#/program1-2.cs
@@ -0,0 +1,16 @@
+using System;
+namespace Hi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var x = 100;
+            var y = -50.2;
+            const float my_pi = 3.1415f;
+            Console.WriteLine("x = " + x);
+            Console.WriteLine("y = " + y);
+            Console.WriteLine("my_pi = " + my_pi);
+        }
+    }
+}
